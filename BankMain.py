e42b9d9d8cdd27f3c2dc0eacdbd6cab2c40637f5
ChetanGorantla <chetangorantla7@gmail.com>
2020-06-19 18:21:48 -0500

    Bank Project

---
+++ BankMain.py
@@ -0,0 +1,22 @@
+# BankMain.py
+import Login
+import Menu
+import Balance
+import Transactions
+import pygame
+from pygame import mixer
+
+# pygame.mixer.init()
+# pygame.mixer.music.load("TheSea.mp3")
+# pygame.mixer.music.play()
+
+print("welcome")
+
+if(Login.performLogin() == True):
+    Menu.displayMenu()
+    # Balance.displayBalance(accountNumber=0)
+else:
+    print("Please try again.")
+    Login.performLogin()
+
+
