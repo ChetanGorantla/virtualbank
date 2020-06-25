5279ccfcd7d46e9bd9e5a8ea34eaa51ce6ed6815
ChetanGorantla <chetangorantla7@gmail.com>
2020-06-24 19:24:58 -0500

    Stocks ticker

--- StockMarket.py
+++ StockMarket.py
@@ -1,5 +1,14 @@
+import Balance
+
 def buyStock():
     print("Check out our latest stocks!")
-
+    ticker = str(input("Please enter your ticker"))
+    quantity = int(input("How much stocks would you like to buy?"))
+    stock_price = 100
+    amount = (stock_price*quantity)
+    Balance.amountToDeduct(amount)
+    print("You have just purchased ", ticker," and the quantity = ", quantity, " total amount paid is ", amount)
+    print("Your account balance is: ", Balance.getBalance())
 def sellStock():
     print("Sell my stocks station.")
+
