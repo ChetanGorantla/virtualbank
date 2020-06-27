6b7bbe6d4c877374c80b3ec51f5750148a6a1628
ChetanGorantla <chetangorantla7@gmail.com>
2020-06-26 19:08:24 -0500

    insurance

--- Insurance.py
+++ Insurance.py
@@ -1,2 +1,10 @@
 def helloInsurance():
     print("Hello")
+
+def insuranceType():
+    typeOfInsurance = str(input("What type of insurance would you like?\n You can have car, house, health, or property insurance.\n"
+                                "1. Car Insurance\n"
+                                "2. House Insurance\n"
+                                "3. Health Insurance\n"
+                                "4. Property Insurance\n"))
+
