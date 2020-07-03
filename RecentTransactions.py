c3a6d7f614b83efc3c72297a085d278ed2fc6c65
ChetanGorantla <chetangorantla7@gmail.com>
2020-07-03 17:46:22 -0500

    update

--- RecentTransactions.py
+++ RecentTransactions.py
@@ -1,15 +1,19 @@
 import random
 import Menu
+import Balance
 
 def display():
-    i = random.randint(50000, 500000)
+    a = random.randint(50000, 500000)
+    b = random.randint(50000, Balance.getBalance())
+    c = random.randint(50000, Balance.getBalance())
+    d = random.randint(50000, 500000)
     look = str(input("Would you like to look at your recent transactions?"))
     if look == "yes" or look == "y":
         print("Ok, here are your most recent transactions:\n",
-              "Deposited $", i,
-              "Withdrew $",i,
-              "Withdrew $",i,
-              "Deposited $",i)
+              "Deposited $", a,
+              "\nWithdrew $",b,
+              "\nWithdrew $",c,
+              "\nDeposited $",d)
         go_back = str(input("Would you like to go back to menu or exit?"))
         if go_back == "go back" or go_back == "back":
             Menu.displayMenu()
